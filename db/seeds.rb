ee6f045434d3006ea1d4b2760a11374dcc8ea56f
Valentin Derrien <derrien.v@gmail.com>
2022-02-16 12:46:37 +0100

    Yelp exercise done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'La Cigale', address: 'Place Graslin 44000 Nantes', category: 'french')
+Restaurant.create(name: 'Plein Sud', address: 'Pornichet', category: 'italian')
+Restaurant.create(name: 'La Plage', address: 'Saint Marc', category: 'french')
+Restaurant.create(name: 'La Tour de Pise', address: 'La Baule', category: 'italian')
+Restaurant.create(name: 'Le Siam', address: 'Saint Herblain', category: 'chinese')
